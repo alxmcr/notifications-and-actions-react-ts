6ff828559b8c9916f9bddf9c4e51f430b943c88f
Alejandro M. Coca <alxmcr@outlook.com>
2023-10-30 13:35:11 -0400

    wip

--- src/components/AppButton/index.tsx
+++ src/components/AppButton/index.tsx
@@ -3,14 +3,20 @@ import './AppButton.scss';
 type AppButtonProps = {
   text: string;
   level: 'success' | 'info' | 'error';
+  handleClick: () => void;
 };
 
 export default function AppButton({
   text = '',
   level = 'info',
+  handleClick,
 }: AppButtonProps) {
   return (
-    <button type="button" className={`app-button app-button--${level}`}>
+    <button
+      type="button"
+      className={`app-button app-button--${level}`}
+      onClick={handleClick}
+    >
       {text}
     </button>
   );
